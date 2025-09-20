24c7d4167d971c5e957025c93110635850c1cb1e
MatheusMorata <matlucar09@hotmail.com>
2025-09-20 14:29:02 -0300

    feat: Adicionei background

--- src/main.c
+++ src/main.c
@@ -1,13 +1,14 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 #define LARGURA 800
 #define ALTURA 600
 
 int main(int args, char* argc[]){
 
-    // Inicializando SDL
     SDL_Init(SDL_INIT_EVERYTHING);
-     
+    IMG_Init(0);
+    
     SDL_Window *janela = SDL_CreateWindow(
         "Ilha Geladeira", // Título
         SDL_WINDOWPOS_CENTERED, // Posição X
@@ -18,10 +19,20 @@ int main(int args, char* argc[]){
 
     SDL_Renderer *renderizador = SDL_CreateRenderer(janela, -1, 0);
 
+    SDL_Texture *capa = IMG_LoadTexture(renderizador, "imgs/capa.png");
+
+    SDL_Rect quadrado = {0, 0, LARGURA, ALTURA};
+
+    SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 0);
+    SDL_RenderClear(renderizador);
+    SDL_RenderCopy(renderizador, capa, NULL, &quadrado);
+    SDL_RenderPresent(renderizador);
+    SDL_Delay(3000);
 
     // Liberando recursos
-    SDL_DestroyRenderer(renderizador)
+    SDL_DestroyRenderer(renderizador);
     SDL_DestroyWindow(janela);
+    IMG_Quit();
     SDL_Quit();
     return 0;
 }
