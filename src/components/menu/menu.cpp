dff0a95bffaf494f52a4abe4a451b66709ae179f
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-09-25 09:53:34 -0300

    refactor: Alterei ordem parâmetros

--- src/components/menu/menu.cpp
+++ src/components/menu/menu.cpp
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-void RenderMenuScreen(SDL_Renderer *renderizador, SDL_Window *janela, SDL_Event evento, Uint32 timeout){
+void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event evento, Uint32 timeout){
   SDL_SetRenderDrawColor(renderizador, 200, 200, 255, 0);
   SDL_RenderClear(renderizador);
   SDL_RenderPresent(renderizador);
