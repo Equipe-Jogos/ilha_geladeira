821f89f5a70483f07a209d35a6005cf600185150
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-09-25 10:01:26 -0300

    feat: Adicionada nova cor

--- src/components/menu/menu.cpp
+++ src/components/menu/menu.cpp
@@ -2,7 +2,7 @@
 #include <SDL2/SDL_image.h>
 
 void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
-  SDL_SetRenderDrawColor(renderizador, 200, 200, 255, 0);
+  SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
   SDL_RenderClear(renderizador);
   SDL_RenderPresent(renderizador);
 }
