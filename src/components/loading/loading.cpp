27fb634effb857cc82d220ede2e5f9765f8b608f
higor.rocha <higor.4446@gmail.com>
2025-09-25 00:52:23 -0300

    fix: ajusta loop de loading

--- src/components/loading/loading.cpp
+++ src/components/loading/loading.cpp
@@ -5,7 +5,7 @@
 #include "../../utils/Aux_Timeout.cpp"
 
 
-void RenderLoadingScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event *evento, Uint32 *timeout) {
+int RenderLoadingScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event *evento, Uint32 *timeout) {
     SDL_Texture *capa = IMG_LoadTexture(renderizador, "imgs/capa.png");
     SDL_Rect img_capa = {0, 0, LARGURA, ALTURA};
     SDL_Rect carregamento = {0, 0, 1, 20};
@@ -13,24 +13,27 @@ void RenderLoadingScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Eve
 
     while (carregando) {
 
-        if (AUX_WaitEventTimeout(evento, timeout)) {
-            if (carregamento.w < 800) {
-                SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 0);
-                SDL_RenderClear(renderizador);
-                SDL_RenderCopy(renderizador, capa, NULL, &img_capa);
-                SDL_SetRenderDrawColor(renderizador, 0, 255, 0, 0);
-                SDL_RenderFillRect(renderizador, &carregamento);
+        if (carregamento.w < 800) {
+            SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 0);
+            SDL_RenderClear(renderizador);
+            SDL_RenderCopy(renderizador, capa, NULL, &img_capa);
+            SDL_SetRenderDrawColor(renderizador, 0, 255, 0, 0);
+            SDL_RenderFillRect(renderizador, &carregamento);
 
-                carregamento.w += 1;
-                SDL_RenderPresent(renderizador);
-            }
-            else {
-                carregando = false;
-            }
+            //if (AUX_WaitEventTimeout(evento, timeout)) {
+            //    if(evento.type == SDL_QUIT){
+            //        carregando = false;
+            //    }
+            //} 
+            carregamento.w += 1;
+            SDL_RenderPresent(renderizador);
+        }else {
+            carregando = false;
         }
     }
 
 
     // Liberando recursos
     SDL_DestroyTexture(capa);
+    return 1;
 }
