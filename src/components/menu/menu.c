ec43950dd11455fa8dcecce7bb4e5b63cdfc1c96
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-09-30 09:59:41 -0300

    feat: Implementando botao

--- src/components/menu/menu.c
+++ src/components/menu/menu.c
@@ -2,15 +2,14 @@
 #include <SDL2/SDL_image.h>
 
 void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
-  char caminhoBotaoJogar[200];
-  char caminhoBotaoSair[200];
   
   IMG_Init(IMG_INIT_PNG);
-  //SDL_Texture *municao = IMG_LoadTexture(renderizador, caminhoBotaoJogar);
-  //SDL_Texture *espada  = IMG_LoadTexture(renderizador, caminhoBotaoSair);
+  SDL_Rect quadradoJogar = {0, 0, 200, 200};
+  SDL_Texture *botaoJogar = IMG_LoadTexture(renderizador, "../src/imgs/jogar.png");
   
   SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
   SDL_RenderClear(renderizador);
+  SDL_RenderCopy(renderer, botaoJogar, NULL, &quadradoJogar);
   SDL_RenderPresent(renderizador);
   
   IMG_Quit();
