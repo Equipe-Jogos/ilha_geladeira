037c172b82b5d758941b060f5cd15fa059e2dafd
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-09-30 09:35:52 -0300

    feat: Inicializando SDL_image

--- src/components/menu/menu.c
+++ src/components/menu/menu.c
@@ -2,7 +2,11 @@
 #include <SDL2/SDL_image.h>
 
 void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
+  IMG_Init(IMG_INIT_PNG);
+  
   SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
   SDL_RenderClear(renderizador);
   SDL_RenderPresent(renderizador);
+  
+  IMG_Quit();
 }
