89af71c08ef0fb9b3a0da0f20921aedeb8c2d1ad
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-09-30 10:16:14 -0300

    feat: Implementando animação botão

--- src/components/menu/menu.c
+++ src/components/menu/menu.c
@@ -1,16 +1,29 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <stdbool.h>
 
 void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
-  
+  bool rodando = true;
   IMG_Init(IMG_INIT_PNG);
   SDL_Rect quadradoJogar = {300, 200, 200, 200};
   SDL_Texture *botaoJogar = IMG_LoadTexture(renderizador, "../src/imgs/jogar.png");
+  SDL_Texture *botaoJogarSelecionado = IMG_LoadTexture(renderizador, "../src/imgs/jogar_selected.png");
   
-  SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
-  SDL_RenderClear(renderizador);
-  SDL_RenderCopy(renderizador, botaoJogar, NULL, &quadradoJogar);
-  SDL_RenderPresent(renderizador);
   
+  while(rodando){
+    int x, y;
+    SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
+    SDL_RenderClear(renderizador);
+    SDL_GetMouseState(&x, &y);
+    SDL_Point *ponto = {x, y};
+  
+    if(SDL_PointInRect(&ponto, &quadradoJogar)){
+      SDL_RenderCopy(renderizador, botaoJogarSelecionado, NULL, &quadradoJogar);  
+    }else{
+      SDL_RenderCopy(renderizador, botaoJogar, NULL, &quadradoJogar); 
+    }
+    
+    SDL_RenderPresent(renderizador);
+  }
   IMG_Quit();
 }
