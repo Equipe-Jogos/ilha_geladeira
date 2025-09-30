65e623faddaa7deb092e960fe4ff03adf287f85e
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-09-30 09:42:52 -0300

    feat: Adicionado estrutras de textura

--- src/components/menu/menu.c
+++ src/components/menu/menu.c
@@ -2,7 +2,12 @@
 #include <SDL2/SDL_image.h>
 
 void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
+  char caminhoBotaoJogar[200];
+  char caminhoBotaoSair[200];
+  
   IMG_Init(IMG_INIT_PNG);
+  //SDL_Texture *municao = IMG_LoadTexture(renderizador, caminhoBotaoJogar);
+  //SDL_Texture *espada  = IMG_LoadTexture(renderizador, caminhoBotaoSair);
   
   SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
   SDL_RenderClear(renderizador);
