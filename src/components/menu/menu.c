5f21dd0e451653e540b2e4a87499d26b9c333bd3
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-10-02 10:30:42 -0300

    feat: Atualizado posição x da cor azul

--- src/components/menu/menu.c
+++ src/components/menu/menu.c
@@ -7,7 +7,7 @@ void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event 
   IMG_Init(IMG_INIT_PNG);
   SDL_Rect vermelhoQ = {500, 200, 100, 100};
   SDL_Rect verdeQ = {550, 200, 100, 100}; 
-  SDL_Rect azulQ = {50, 500, 100, 100};
+  SDL_Rect azulQ = {600, 200, 100, 100};
   
   SDL_Texture* vermelho_img = IMG_LoadTexture(renderizador, "../src/imgs/cor_vermelho.png");
   SDL_Texture* verde_img = IMG_LoadTexture(renderizador, "../src/imgs/cor_verde_medio.png");
