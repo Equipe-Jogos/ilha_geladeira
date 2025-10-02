93021fd53ba1fc894e2bcf3904509a03ec9b748d
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-10-02 10:09:04 -0300

    feat: Adicionado novos botões no menu

--- src/components/menu/menu.c
+++ src/components/menu/menu.c
@@ -5,12 +5,19 @@
 void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
 
   IMG_Init(IMG_INIT_PNG);
-  SDL_Rect quadradoJogar = {300, 200, 200, 200};
-  SDL_Texture *botaoJogar = IMG_LoadTexture(renderizador, "../src/imgs/b1.png"); 
+  SDL_Rect vermelhoQ = {300, 200, 50, 50};
+  SDL_Rect verdeQ = {350, 250, 50, 50}; 
+  SDL_Rect azulQ = {350, 250, 50, 50};
+  
+  SDL_Texture* vermelho_img = IMG_LoadTexture(renderizador, "../src/imgs/cor_vermelho.png");
+  SDL_Texture* verde_img = IMG_LoadTexture(renderizador, "../src/imgs/cor_verde.png");
+  SDL_Texture* azul_img = IMG_LoadTexture(renderizador, "../src/imgs/cor_azul.png");
   
   SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
   SDL_RenderClear(renderizador);
-  SDL_RenderCopy(renderizador, botaoJogar, NULL, &quadradoJogar); 
+  SDL_RenderCopy(renderizador, vermelho_img, NULL, &vermelhoQ);
+  SDL_RenderCopy(renderizador, verde_img, NULL, &verdeQ);
+  SDL_RenderCopy(renderizador, azul_img, NULL, &azulQ);
   SDL_RenderPresent(renderizador);
 
   IMG_Quit();
