1c58c7a724fc59ce9c9bad4822ff3f7991f8aae2
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-10-02 10:21:25 -0300

    feat: Adicionado novo elemento de cor

--- src/components/menu/menu.c
+++ src/components/menu/menu.c
@@ -5,19 +5,19 @@
 void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
 
   IMG_Init(IMG_INIT_PNG);
-  SDL_Rect vermelhoQ = {300, 200, 50, 50};
-  SDL_Rect verdeQ = {500, 250, 50, 50}; 
-  //SDL_Rect azulQ = {350, 250, 50, 50};
+  SDL_Rect vermelhoQ = {300, 200, 100, 100};
+  SDL_Rect verdeQ = {500, 250, 100, 100}; 
+  SDL_Rect azulQ = {50, 500, 100, 100};
   
   SDL_Texture* vermelho_img = IMG_LoadTexture(renderizador, "../src/imgs/cor_vermelho.png");
   SDL_Texture* verde_img = IMG_LoadTexture(renderizador, "../src/imgs/cor_verde_medio.png");
-  //SDL_Texture* azul_img = IMG_LoadTexture(renderizador, "../src/imgs/cor_azul.png");
+  SDL_Texture* azul_img = IMG_LoadTexture(renderizador, "../src/imgs/cor_azul.png");
   
   SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
   SDL_RenderClear(renderizador);
   SDL_RenderCopy(renderizador, vermelho_img, NULL, &vermelhoQ);
   SDL_RenderCopy(renderizador, verde_img, NULL, &verdeQ);
-  //SDL_RenderCopy(renderizador, azul_img, NULL, &azulQ);
+  SDL_RenderCopy(renderizador, azul_img, NULL, &azulQ);
   SDL_RenderPresent(renderizador);
 
   IMG_Quit();
