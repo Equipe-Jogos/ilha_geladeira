88d34545cf39738c2766369e0403723e399a9f6c
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-10-02 09:53:50 -0300

    feat: Adicionando novo botão

--- src/components/menu/menu.c
+++ src/components/menu/menu.c
@@ -3,27 +3,15 @@
 #include <stdbool.h>
 
 void RenderMenuScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
-  bool rodando = true;
+
   IMG_Init(IMG_INIT_PNG);
   SDL_Rect quadradoJogar = {300, 200, 200, 200};
-  SDL_Texture *botaoJogar = IMG_LoadTexture(renderizador, "../src/imgs/jogar.png");
-  SDL_Texture *botaoJogarSelecionado = IMG_LoadTexture(renderizador, "../src/imgs/jogar_selected.png");
+  SDL_Texture *botaoJogar = IMG_LoadTexture(renderizador, "../src/imgs/b1.png"); 
   
-  
-  while(rodando){
-    int x, y;
-    SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
-    SDL_RenderClear(renderizador);
-    SDL_GetMouseState(&x, &y);
-    SDL_Point ponto = {x, y};
-  
-    if(SDL_PointInRect(&ponto, &quadradoJogar)){
-      SDL_RenderCopy(renderizador, botaoJogarSelecionado, NULL, &quadradoJogar);  
-      SDL_RenderPresent(renderizador);
-    }else{
-      SDL_RenderCopy(renderizador, botaoJogar, NULL, &quadradoJogar); 
-      SDL_RenderPresent(renderizador);
-    }
-  }
+  SDL_SetRenderDrawColor(renderizador, 36, 116, 255, 0);
+  SDL_RenderClear(renderizador);
+  SDL_RenderCopy(renderizador, botaoJogar, NULL, &quadradoJogar); 
+  SDL_RenderPresent(renderizador);
+
   IMG_Quit();
 }
