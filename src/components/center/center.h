85feed5ab94487a097c37162db12b499054ed560
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-10-14 10:23:33 -0300

    feat: Atualizado função RenderCenterScreen

--- src/components/center/center.h
+++ src/components/center/center.h
@@ -7,8 +7,15 @@
 #include "../../utils/Aux_Timeout.h"
 
 inline int RenderCenterScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
+  int LARGURA, ALTURA;
   IMG_Init(IMG_INIT_PNG);
-  SDL_Texture* textura_iniciar = IMG_LoadTexture(renderizador, "../src/imgs/centro.png");
+  SDL_Texture *centroIMG = IMG_LoadTexture(renderizador, "../src/imgs/centro.png");
+  obterTamanhoJanela(janela, &LARGURA, &ALTURA);
+  SDL_Rect centroRect = {pos_botao_x, pos_botao_y, tam_botao_x, tam_botao_y};
+
+  SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 0);
+  SDL_RenderClear(renderizador);
+  SDL_RenderCopy(renderizador, centroIMG, NULL, &);
   
   IMG_Quit();
   return 1;
