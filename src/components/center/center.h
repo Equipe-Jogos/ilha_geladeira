6940c2f5dfd2fa0a5e68277308927d4d54eeebd2
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-10-14 10:18:42 -0300

    feat: Iniciando função RenderCenterScreen

--- src/components/center/center.h
+++ src/components/center/center.h
@@ -1,1 +1,16 @@
+#ifndef CENTER_H
+#define CENTER_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdbool.h>
+#include "../../utils/Aux_Timeout.h"
+
+inline int RenderCenterScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout){
+  IMG_Init(IMG_INIT_PNG);
+  SDL_Texture* textura_iniciar = IMG_LoadTexture(renderizador, "../src/imgs/centro.png");
+  
+  IMG_Quit();
+  return 1;
+}
 
