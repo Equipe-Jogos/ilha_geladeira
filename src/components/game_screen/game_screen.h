e2d53bbaf435ec5c5eff46d355f9f28128564be7
MatheusMorata <matlucar09@hotmail.com>
2025-11-02 16:45:54 -0300

    feat: Implementando algumas mudanças de cor

--- src/components/game_screen/game_screen.h
+++ src/components/game_screen/game_screen.h
@@ -12,13 +12,54 @@
 static inline int RenderGameScreen(SDL_Window *janela, SDL_Renderer *renderizador, SDL_Event * evento, Uint32 *timeout, GameState *estadoJogo) {
     int LARGURA, ALTURA;
     int mouse_x, mouse_y;
+    char caminho[100];
     obterTamanhoJanela(janela, &LARGURA, &ALTURA);
     SDL_GetMouseState(&mouse_x, &mouse_y);
 
     IMG_Init(IMG_INIT_PNG);
     SDL_Texture *centroIMG = IMG_LoadTexture(renderizador, "imgs/centro.png");
-    SDL_Texture *textura_pinguim_0 = IMG_LoadTexture(renderizador, "imgs/pinguim/azul/000.png");
-      
+    switch (corSelecionada) {
+        case 0:
+            sprintf(caminho, "imgs/pinguim/amarelo/000.png");
+            break;
+        case 1:
+            sprintf(caminho, "imgs/pinguim/rosa/000.png");
+            break;
+        case 2:
+            sprintf(caminho, "imgs/pinguim/azul/000.png");
+            break;
+        case 3:
+            sprintf(caminho, "imgs/pinguim/azul_claro/000.png");
+            break;
+        case 4:
+            SDL_Log("cinza");
+            sprintf(caminho, "imgs/pinguim/cinza/000.png");
+            break;
+        case 5:
+            sprintf(caminho, "imgs/pinguim/branco/000.png");
+            break;
+        case 6:
+            sprintf(caminho, "imgs/pinguim/roxo/000.png");
+            break;
+        case 7:
+            sprintf(caminho, "imgs/pinguim/rosa/000.png");
+            break;
+        case 8:
+            sprintf(caminho, "imgs/pinguim/laranja/000.png");
+            break;
+        case 9:
+            sprintf(caminho, "imgs/pinguim/cinza/000.png");
+            break;
+        case 10:
+            sprintf(caminho, "imgs/pinguim/marrom/000.png");
+            break;
+        case 11:
+            sprintf(caminho, "imgs/pinguim/ciano/000.png");
+            break;
+    }
+
+    SDL_Texture *textura_pinguim_0 = IMG_LoadTexture(renderizador, caminho);
+  
     int destino_x = floor(LARGURA/2), destino_y = floor(ALTURA/2);
     SDL_Rect pinguimRect = {floor(LARGURA/2), floor(ALTURA/2), 100, 160};
     SDL_Rect centroRect = {0, 0, LARGURA, ALTURA};
