c21d88e12195af871b97a5ed84b7c3c63bf5fc2a
samuel <samuel.rocha.castan@gmail.com>
2025-11-21 15:27:53 +0000

    feat: implementando bean counters

--- src/components/Bean Counters/bean_counters.h
+++ src/components/Bean Counters/bean_counters.h
@@ -5,9 +5,13 @@
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 #include "../../utils/Aux_Timeout.h"
 #include "../../utils/Aux_monitor.h"
-#include "../personalizacao/personalizacao.h" 
+#include "../personalizacao/personalizacao.h"
+
+//Dimensões da Tela
+int LARGURA, ALTURA;
 
 // Gravidade da carga
 double gravidade = 800.0;
@@ -15,17 +19,37 @@ double gravidade = 800.0;
 // Marcadores de tempo
 Uint32 tempo_anterior_carga = 0;
 
-enum 
+//limites de movimentao do pinguim
+int limite_esq;
+int limite_dir;
+
+// Estados de movimento do pinguim
+typedef enum 
 {
     PARADO,
     MOVENDO
-} Estados_movimento;
+} Estados_pinguim_movimento;
 
+
+typedef enum
+{
+    NAO_CARREGA,
+    CARREGA1,
+    CARREGA2,
+    CARREGA3,
+    CARREGA4,
+    PESO_MAXIMO,
+    DANO
+
+} Estados_pinguim_carga;
+
+// Tipos de carga
 enum
 {
     GRAOS,
     PEIXE,
-    BIGORNA
+    BIGORNA,
+    VASO
 } Enum_carga;
 
 
@@ -35,17 +59,135 @@ enum
 typedef struct 
 {
     SDL_Rect rect;
-    char * txt;
-    int peso;
+    SDL_Texture * txt;
+    int tipo;
 
     double pos_x;
     double pos_y;
 
     double velocidade_x;
     double velocidade_y;
+    Uint32 tempo_anterior_carga;
 
 } Carga;
 
+//Vetor de cargas
+typedef struct 
+{
+    Carga * cargas;
+    int tamanho;
+    int ultimo_index;
+    int prob;
+
+
+} Vetor_carga;
+
+typedef struct 
+{
+    SDL_Texture  * txt;
+    SDL_Rect rect;
+    Estados_pinguim_carga estado_carga;
+    Estados_pinguim_movimento estado_movimento;
+    double velocidade;
+    double posx;
+
+} Pinguim;
+
+void inicializa_pinguim(SDL_Renderer * renderizador, Pinguim * pinguim)
+{
+    int comprimento_andavel = limite_dir - limite_esq;
+    int posicao_inicial = limite_esq + comprimento_andavel / 2 - 150;
+    pinguim->txt = IMG_LoadTexture(renderizador, "imgs/bean_counters/penguin.webp");
+    pinguim->rect = (SDL_Rect){ posicao_inicial, ALTURA - 200, 150, 150 };
+    pinguim->estado_movimento = PARADO;
+    pinguim->estado_carga = NAO_CARREGA;
+    pinguim->velocidade;
+    pinguim->posx = 0;
+
+}
+
+void atualiza_estado_pinguim()
+{
+
+}
+
+void inicializa_vetor_carga(Vetor_carga * vetor,int tam)
+{
+    vetor->tamanho = tam;
+    vetor->cargas = malloc(tam*sizeof(Carga));
+    vetor->ultimo_index = -1;
+}
+
+void inicializa_carga(SDL_Renderer * renderizador, Carga * carga)
+{
+    carga->rect = (SDL_Rect){ LARGURA * 0.8, ALTURA - 400, 70, 60 };
+    carga->pos_x = carga->rect.x;
+    carga->pos_y = carga->rect.y;
+    carga->velocidade_x = -200;
+    carga->tempo_anterior_carga = 0;
+
+    double space_x = limite_esq + rand() % (limite_dir - limite_esq + 1);
+    double space_y = ALTURA - 100;
+
+    double tempo = (space_x - carga->rect.x) / carga->velocidade_x;
+
+    if (tempo <= 0.01) {
+        printf("Movimento impossível! tempo=%f\n", tempo);
+        return;
+    }
+
+    double dy = space_y - carga->pos_y;
+
+    // 🔥 FÓRMULA CORRETA DE VELOCIDADE INICIAL PARA ALCANÇAR Y EM T TEMPO
+    carga->velocidade_y = (2 * dy - gravidade * tempo * tempo) / (2 * tempo);
+
+    carga->tipo = rand()%4;
+    
+    switch (carga->tipo)
+    {
+        case GRAOS:
+            carga->txt = IMG_LoadTexture(renderizador, "imgs/bean_counters/Coffe_bag.webp");
+            break;
+        case PEIXE:
+            carga->txt = IMG_LoadTexture(renderizador, "imgs/bean_counters/Fish_bean_counters.webp");
+            break;
+        case BIGORNA:
+            carga->txt = IMG_LoadTexture(renderizador, "imgs/bean_counters/Anvil.webp");
+            break;
+        case VASO:
+            carga->txt = IMG_LoadTexture(renderizador, "imgs/bean_counters/Flower_pot.webp");
+            break;
+        default:
+            break;
+    }
+
+
+
+    //printf("%f,%d, %f, %f\n", space_x, carga->rect.x, tempo, carga->velocidade_y);
+}
+
+
+void sorteia_carga(SDL_Renderer *renderizador, Vetor_carga * vetor)
+{
+    vetor->prob = rand()%60;
+    if (vetor->tamanho > vetor->ultimo_index)
+    {
+        if (vetor->prob == 13)
+        {
+            printf("%d\n",vetor->ultimo_index);
+            inicializa_carga(renderizador,&vetor->cargas[++vetor->ultimo_index]);
+        }
+    }
+}
+
+void draw_cargas(SDL_Renderer * renderizador,Vetor_carga * vetor)
+{
+        for (int i = 0; i <= vetor->ultimo_index; i++)
+        {
+            SDL_RenderCopy(renderizador, vetor->cargas[i].txt, NULL, &vetor->cargas[i].rect);
+            //SDL_RenderFillRect(renderizador, &vetor->cargas[i].rect);
+        }
+}
 
 // ==================================================
 //   FÍSICA DA CARGA — DT CORRETO, SEM MUDAR PINGUIM
@@ -54,11 +196,11 @@ void calcula_movimento_carga(Carga *carga)
 {
     Uint32 agora = SDL_GetTicks();
 
-    if (tempo_anterior_carga == 0)
-        tempo_anterior_carga = agora;
+    if (carga->tempo_anterior_carga == 0)
+        carga->tempo_anterior_carga = agora;
 
-    double dt = (agora - tempo_anterior_carga) / 1000.0;
-    tempo_anterior_carga = agora;
+    double dt = (agora - carga->tempo_anterior_carga) / 1000.0;
+    carga->tempo_anterior_carga = agora;
 
     // Física REAL
     carga->velocidade_y += gravidade * dt;
@@ -69,6 +211,15 @@ void calcula_movimento_carga(Carga *carga)
     carga->rect.y = (int)carga->pos_y;
 }
 
+
+void calcula_movimento_cargas(Vetor_carga * vetor)
+{
+    for (int i=0;i<= vetor->ultimo_index; i++)
+    {
+        calcula_movimento_carga(&vetor->cargas[i]);
+    }
+
+}
 
 
 // ==================================================
@@ -81,7 +232,9 @@ static inline int RenderBeanCountersScreen(
     Uint32 *timeout,
     GameState *estadoJogo)
 {
-    int LARGURA, ALTURA;
+
+    srand(time(NULL));
+
     SDL_Point inicial_mouse;
     SDL_Point atual_mouse;
 
@@ -93,8 +246,8 @@ static inline int RenderBeanCountersScreen(
     obterTamanhoJanela(janela, &LARGURA, &ALTURA);
     SDL_GetMouseState(&inicial_mouse.x, &inicial_mouse.y);
 
-    int limite_esq = LARGURA * 0.25;
-    int limite_dir = LARGURA * 0.75;
+    limite_esq = LARGURA * 0.25;
+    limite_dir = LARGURA * 0.75;
 
     int comprimento_andavel = limite_dir - limite_esq;
     int posicao_inicial = limite_esq + comprimento_andavel / 2;
@@ -106,17 +259,15 @@ static inline int RenderBeanCountersScreen(
 
     // Pinguim
     SDL_Rect pinguim = { posicao_inicial, ALTURA - 100, 30, 30 };
+    Pinguim p2;
+    inicializa_pinguim(renderizador, &p2);
 
     // Caminhão
     SDL_Rect caminhao = { LARGURA * 0.80, ALTURA - 600, 300, 500 };
 
     // CARGA
-    Carga carga;
-    carga.rect = (SDL_Rect){ LARGURA * 0.85, ALTURA - 400, 30, 30 };
-    carga.pos_x = carga.rect.x;
-    carga.pos_y = carga.rect.y;
-    carga.velocidade_x = -200;
-    carga.velocidade_y = -500;
+    Vetor_carga cargas_jogadas;
+    inicializa_vetor_carga(&cargas_jogadas,200);
 
     SDL_Point esq = { limite_esq, ALTURA - 100 };
     SDL_Point dir = { limite_dir, ALTURA - 100 };
@@ -140,6 +291,7 @@ static inline int RenderBeanCountersScreen(
             {
                 *estadoJogo = STATE_MENU;
                 SDL_ShowCursor(SDL_ENABLE);
+                free(cargas_jogadas.cargas);
                 IMG_Quit();
                 return 1;
             }
@@ -181,7 +333,11 @@ static inline int RenderBeanCountersScreen(
         // ============================
         //  MOVIMENTO DA CARGA (FÍSICA)
         // ============================
-        calcula_movimento_carga(&carga);
+
+        sorteia_carga(renderizador, &cargas_jogadas);
+        calcula_movimento_cargas(&cargas_jogadas);
+       
+
 
 
         // ============================
@@ -213,7 +369,7 @@ static inline int RenderBeanCountersScreen(
         SDL_Rect destino_rect = { destino, ALTURA - 100, 10, 10 };
 
         SDL_SetRenderDrawColor(renderizador, 0, 0, 255, 255);
-        SDL_RenderFillRect(renderizador, &carga.rect);
+        draw_cargas(renderizador, &cargas_jogadas);
 
         SDL_SetRenderDrawColor(renderizador, 0, 255, 255, 255);
         SDL_RenderFillRect(renderizador, &pinguim);
@@ -226,6 +382,8 @@ static inline int RenderBeanCountersScreen(
 
         SDL_RenderDrawPoint(renderizador, esq.x, esq.y);
         SDL_RenderDrawPoint(renderizador, dir.x, dir.y);
+
+         SDL_RenderCopy(renderizador, p2.txt, NULL, &p2.rect);
 
         SDL_RenderPresent(renderizador);
     }
