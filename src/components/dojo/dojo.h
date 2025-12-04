be831692e5d30be15bd6be5c26a17d1989ae395d
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-12-04 09:31:05 -0300

    feat: Iniciando implementação do minigame dojo

--- src/components/dojo/dojo.h
+++ src/components/dojo/dojo.h
@@ -1,1 +1,25 @@
+#ifndef DOJO_H
+#define DOJO_H
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include "../../utils/Aux_Timeout.h"
+#include "../../utils/Aux_monitor.h"
+#include "../personalizacao/personalizacao.h"
+
+static inline int RenderDojo(
+    SDL_Window *janela, 
+    SDL_Renderer *renderizador, 
+    SDL_Event * evento, 
+    Uint32 *timeout,
+    GameState *estadoJogo  
+    )
+{
+    
+    srand(time(NULL));
+}
+#endif
