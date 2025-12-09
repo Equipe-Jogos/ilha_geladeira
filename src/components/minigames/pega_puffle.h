f609d934f19310b1d69d20fe3065d7ca00482181
higor.rocha <higor.4446@gmail.com>
2025-12-08 23:51:14 -0300

    feat: implementa lógica de colisão e verificação de área no minigame Pega Puffle
    - Adicionadas funções para verificar colisões entre retângulos e se os puffles estão dentro de uma área cercada.
    - Aumentado o número de puffles de 3 para 10 e ajustadas as posições iniciais aleatórias.
    - Implementada lógica para reverter movimento dos puffles em caso de colisão e verificar se todos os puffles estão dentro do cercado para finalizar o jogo.

--- src/components/minigames/pega_puffle.h
+++ src/components/minigames/pega_puffle.h
@@ -5,6 +5,7 @@
 #include <SDL2/SDL_image.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include "../../utils/Aux_monitor.h"
 #include "../../utils/Aux_Timeout.h"
 #include "../../utils/Aux_movimentacao.h"
@@ -14,6 +15,18 @@ typedef struct {
     float x;
     float y;
 } Velocidade ;
+
+static inline int VerificaColisaoRect(SDL_Rect a, SDL_Rect b) {
+    return (a.x < b.x + b.w && a.x + a.w > b.x &&
+            a.y < b.y + b.h && a.y + a.h > b.y);
+}
+
+static inline int VerificaPuffleDentroCercado(SDL_Rect puffle, SDL_Rect cercado) {
+    return (puffle.x >= cercado.x &&
+            puffle.y >= cercado.y &&
+            puffle.x + puffle.w <= cercado.x + cercado.w &&
+            puffle.y + puffle.h <= cercado.y + cercado.h);
+}
 
 static inline int RenderPegaPuffleScreen(
     SDL_Window *janela, 
@@ -29,7 +42,7 @@ static inline int RenderPegaPuffleScreen(
     SDL_Texture *fundo_img = IMG_LoadTexture(renderizador, "imgs/puffle_roundup/fundo.png");
     SDL_Texture *arvores_img = IMG_LoadTexture(renderizador, "imgs/puffle_roundup/arvores.png");
 
-    int num_puffles = 3;
+    int num_puffles = 10;
     int altura_puffle = ALTURA/18;
     int largura_puffle = ALTURA/18;
 
@@ -50,9 +63,12 @@ static inline int RenderPegaPuffleScreen(
         puffle_textures[i] = IMG_LoadTexture(renderizador, puffle_paths[i]);
     }
 
-    SDL_Rect hitbox1 = {17*LARGURA/40, ALTURA/2, LARGURA/30, ALTURA/4};
-    SDL_Rect hitbox2 = {10*LARGURA/24, 3*ALTURA/4, 2*LARGURA/11, ALTURA/15}; 
-    SDL_Rect hitbox3 = {45*LARGURA/80, ALTURA/2, LARGURA/30, ALTURA/4};
+    SDL_Rect hitboxes[3] = {
+        {17*LARGURA/40, ALTURA/2, LARGURA/30, ALTURA/4},
+        {10*LARGURA/24, 3*ALTURA/4, 2*LARGURA/11, ALTURA/15},
+        {45*LARGURA/80, ALTURA/2, LARGURA/30, ALTURA/4}
+    };
+    SDL_Rect cercado = {17*LARGURA/40, 6*ALTURA/10, 2*LARGURA/13, 3*ALTURA/10};
 
     SDL_Rect cenario_rect = {0, 0, LARGURA, ALTURA};
     SDL_Point_Float posicoes_puffles[num_puffles];
@@ -64,10 +80,8 @@ static inline int RenderPegaPuffleScreen(
 
     srand(time(NULL));
     for (int i = 0; i < num_puffles; i++) {     
-        int range_x = (2 * LARGURA / 3) - (LARGURA / 3);
-        int range_y = (2 * ALTURA / 3) - (ALTURA / 3);
-        int x_random = (LARGURA / 3) + (rand() % (range_x + 1));
-        int y_random = (ALTURA / 3) + (rand() % (range_y + 1));
+        int x_random = (LARGURA / 3) + (rand() % ((LARGURA / 3) + 1));
+        int y_random = (ALTURA / 10) + (rand() % ((3*ALTURA / 10) + 1));
         puffle_rects[i] = (SDL_Rect){x_random, y_random, altura_puffle, largura_puffle};
         posicoes_puffles[i] = (SDL_Point_Float){(float)puffle_rects[i].x, (float)puffle_rects[i].y};
         destinos_puffles[i] = (SDL_Point_Float){(float)puffle_rects[i].x, (float)puffle_rects[i].y};
@@ -142,6 +156,10 @@ static inline int RenderPegaPuffleScreen(
         // Atualiza posições dos puffles continuamente
         for (int i = 0; i < num_puffles; i++) {
             if (distancias[i] > 0.5f && distancias[i] <= 200) {
+                // Salva posição anterior para reverter em caso de colisão
+                float pos_x_anterior = posicoes_puffles[i].x;
+                float pos_y_anterior = posicoes_puffles[i].y;
+                
                 SDL_Point nova_posicao = AtualizaPosicao(
                     &posicoes_puffles[i].x, 
                     &posicoes_puffles[i].y, 
@@ -154,7 +172,43 @@ static inline int RenderPegaPuffleScreen(
     
                 puffle_rects[i].x = nova_posicao.x;
                 puffle_rects[i].y = nova_posicao.y;
+                
+                // Verifica colisão com todas as hitboxes
+                int colidiu = 0;
+                for (int j = 0; j < 3; j++) {
+                    if (VerificaColisaoRect(puffle_rects[i], hitboxes[j])) {
+                        colidiu = 1;
+                        break;
+                    }
+                }
+                
+                // Se colidiu, reverte movimento e para o puffle
+                if (colidiu) {
+                    posicoes_puffles[i].x = pos_x_anterior;
+                    posicoes_puffles[i].y = pos_y_anterior;
+                    puffle_rects[i].x = (int)roundf(pos_x_anterior);
+                    puffle_rects[i].y = (int)roundf(pos_y_anterior);
+                    velocidades[i].x = 0;
+                    velocidades[i].y = 0;
+                    distancias[i] = 0;
+                }
             }
+        }
+
+        // Verifica se todos os puffles estão dentro do cercado
+        int todos_dentro = 1;
+        for (int i = 0; i < num_puffles; i++) {
+            if (!VerificaPuffleDentroCercado(puffle_rects[i], cercado)) {
+                todos_dentro = 0;
+                break;
+            }
+        }
+        
+        // Se todos os puffles estão dentro do cercado, termina o jogo
+        if (todos_dentro) {
+            *estadoJogo = STATE_JOGANDO;
+            IMG_Quit();
+            return 1;
         }
 
         SDL_RenderPresent(renderizador);
